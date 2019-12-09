e824d36a94fb30eb32a70424419f188fbf151cca
yarastqt <yarastqt@gmail.com>
2019-12-09 17:14:33 +0300

    feat: add plugin stub

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world')
+import postcss from 'postcss'
+
+type ThemeFoldOptions = {
+  themes: any
+}
+
+module.exports = postcss.plugin<ThemeFoldOptions>('postcss-theme-fold', (options) => {
+  return async (root) => {}
+})
