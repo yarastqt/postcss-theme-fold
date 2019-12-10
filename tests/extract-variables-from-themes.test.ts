5b04887e2bf14891bc311de56261c036687aefa2
yarastqt <yarastqt@gmail.com>
2019-12-10 12:35:36 +0300

    test: add unit for variable extractor from themes

---
+++ tests/extract-variables-from-themes.test.ts
@@ -0,0 +1,24 @@
+import { extractVariablesFromThemes } from '../src/extract-variables-from-themes'
+import { resolveFixture } from './__internal/fixture-resolver'
+
+describe('extract-variables-from-themes', () => {
+  test('should return set of map with themes variables', async (done) => {
+    const themes = await extractVariablesFromThemes([
+      [resolveFixture('components/Theme/presets/b.css')],
+    ])
+    const expected = new Set([
+      new Map([
+        ['.Theme_color_b', new Map([
+          ['--color-1', '#000'],
+          ['--color-2', 'rgb(38, 38, 38)'],
+        ])],
+        ['.Theme_cosmetic_a', new Map([
+          ['--cosmetic-1', '2px'],
+          ['--cosmetic-2', '4px'],
+        ])],
+      ]),
+    ])
+    expect(themes).toEqual(expected)
+    done()
+  })
+})
